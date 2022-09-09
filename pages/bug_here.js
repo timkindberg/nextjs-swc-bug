67cd73c90f566e166212f151acc277c681777249
Tim Kindberg <tim.kindberg@workday.com>
2022-09-09 14:14:53 -0400

    Recreate bug caused by SWC transpiling
    Error "Module parse failed: Duplicate export '_ref'"

---
+++ pages/bug_here.js
@@ -0,0 +1,9 @@
+const fn = fn => []
+
+export const [JobDetailsProvider, useJobDetails] = fn(() => {
+  const { ...query } = {}
+})
+
+export const [CandidatesProvider, useCandidates] = fn(() => {
+  const { ...query } = {}
+})
